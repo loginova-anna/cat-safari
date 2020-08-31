43b669e46648a56067e62581f3b8d9c1e10b8c22
Anna Loginova <anna.loginova@cint.com>
2020-08-31 23:47:23 +0200

    some pages no styling

---
+++ safari-friends.js
@@ -0,0 +1,10 @@
+const isSafari = navigator.userAgent.toLowerCase().indexOf('safari') != -1
+                && navigator.userAgent.toLowerCase().indexOf('chrome') == -1;
+const margin = isSafari ? 3 : 1;
+const place = (Math.round(Math.random()) + margin);
+let infoId = 'info' + place;
+let emptyId = 'empty' + place;
+let infoElement = document.getElementById(infoId);
+let emptyElement = document.getElementById(emptyId);
+infoElement.classList.remove('hidden');
+emptyElement.classList.add('hidden');
